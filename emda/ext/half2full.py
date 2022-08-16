3dd1aa2df5a3173d26930050dd857aa19dc28626
Rangana Warshamanage <ranganaw@mrc-lmb.cam.ac.uk>
2022-08-16 16:19:32 +0100

    combine in real space

--- emda/ext/half2full.py
+++ emda/ext/half2full.py
@@ -11,9 +11,13 @@ import numpy as np
 # Fullmap from halfmaps
 
 
+#def half2full(ar1, ar2):
+#    assert ar1.shape == ar2.shape
+#    hf1 = np.fft.fftn(ar1)
+#    hf2 = np.fft.fftn(ar2)
+#    return np.real(np.fft.ifftn((hf1 + hf2) / 2.0))
+
 def half2full(ar1, ar2):
     assert ar1.shape == ar2.shape
-    hf1 = np.fft.fftn(ar1)
-    hf2 = np.fft.fftn(ar2)
-    return np.real(np.fft.ifftn((hf1 + hf2) / 2.0))
+    return (ar1 + ar2) / 2.0
 
